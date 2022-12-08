b7f0c9d34c49136aff8e4cb02f25919b305a4aa4
Joseph Astier <jastier@gmail.com>
2022-12-08 09:01:00 -0700

    Added a test file

---
+++ skema/skema-rs/comment_extraction/tests/data/test.py
@@ -0,0 +1,25 @@
+
+### Free range comment
+
+'''Free range docstring '''
+
+class OuterClass:
+
+    '''OuterClass docstring '''
+
+    class InnerClass:
+        '''InnerClass docstring '''
+
+        # inner_function comment
+        def inner_function():
+            '''inner_function docstring.'''
+            return None
+
+        class InnerInnerClass:
+            '''InnerInnerClass docstring '''
+
+    # outer_function comment
+    def outer_function():
+        '''outer_function docstring.'''
+        return None
+
