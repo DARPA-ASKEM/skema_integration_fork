0e358c4f3c50000eb9648b2b38224dd360745d27
Vincent Raymond <vincentraymond1995@gmail.com>
2023-01-25 14:01:58 -0500

    Making blobs required

--- skema/code2fn/defined_types.py
+++ skema/code2fn/defined_types.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 
 class System(BaseModel):
     files: Optional[List[str]] = None
-    blobs: Optional[List[str]]
+    blobs: List[str]
     system_name: Optional[str] = ""
     root_name: Optional[str] = ""
